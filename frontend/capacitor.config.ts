42d088f0eba6780c19c78789e068dc4386e9dcb3
farhanbin65 <138449658+farhanbin65@users.noreply.github.com>
2026-06-29 19:55:36 +0100

    update: ios project test

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -1,11 +1,12 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
-  appId: 'com.finsight.app',
+  appId: 'com.farhanhossain.finsight',
   appName: 'FinSight',
   webDir: 'dist',
   server: {
     url: 'https://finance-tracker-five-umber.vercel.app',
     cleartext: true,
+    allowNavigation: ['*']
   }
 };
 export default config;
